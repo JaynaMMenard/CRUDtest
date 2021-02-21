b04a41b4e3c047cd9a2578be584d13a5d529d266
JaynaMMenard <jaynamenard@gmail.com>
2021-02-21 17:27:20 -0500

    First Commit

---
+++ src/main/java/com/hypeform/hypeformTest/HypeformTestApplication.java
@@ -0,0 +1,13 @@
+package com.hypeform.hypeformTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HypeformTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HypeformTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hypeform/hypeformTest/HypeformTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hypeform.hypeformTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HypeformTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
