e5c7b48d1493309913192b5c70230f9c146e4a53
JaynaMMenard <jaynamenard@gmail.com>
2021-02-23 22:08:59 -0500

    Configuration of Firestore SDK. Boilercode from fb docs.

---
+++ src/main/java/com/hypeform/hypeformTest/util/firebaseInitializer.java
@@ -0,0 +1,29 @@
+package com.hypeform.hypeformTest.util;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class firebaseInitializer {
+
+    @PostConstruct
+    private void initFirestore() throws IOException {
+
+        FileInputStream serviceAccount =
+                new FileInputStream("path/to/serviceAccountKey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+
+    }
+
+
+}
